1479a0a38ff4ff60df72c5181b0280df7c313f98
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-02-16 23:39:55 -0800

    2022 - day 3

---
+++ 2022/day03/part1.ts
@@ -0,0 +1,17 @@
+import { readInput } from "../../utils";
+import { getUniqueChars, getItemPriority } from "./common";
+
+export const part1 = async () => {
+  const input = await readInput(__dirname);
+
+  let sum = 0;
+  input.forEach((line) => {
+    const compartment1 = getUniqueChars(line.slice(0, line.length / 2));
+    const compartment2 = getUniqueChars(line.slice(line.length / 2));
+    const shared = [...compartment2].filter((s) => compartment1.has(s));
+    sum += shared.reduce((n, s) => n + getItemPriority(s), 0);
+  });
+  console.log(sum);
+};
+
+(async () => part1())();
