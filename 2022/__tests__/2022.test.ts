2daf380bb92dfb2f8db91c5c5b9488ddf488c44d
Sunil Pandey <64631561+sunilbpandey@users.noreply.github.com>
2023-02-21 10:29:50 -0800

    Add unit tests

---
+++ 2022/__tests__/2022.test.ts
@@ -0,0 +1,43 @@
+import { solve } from "../solve";
+
+const solutions = [
+  { day: 1, part: 1, solution: "67016" },
+  { day: 1, part: 2, solution: "200116" },
+  { day: 2, part: 1, solution: "14163" },
+  { day: 2, part: 2, solution: "12091" },
+  { day: 3, part: 1, solution: "7908" },
+  { day: 3, part: 2, solution: "2838" },
+  { day: 4, part: 1, solution: "605" },
+  { day: 4, part: 2, solution: "914" },
+  { day: 5, part: 1, solution: "DHBJQJCCW" },
+  { day: 5, part: 2, solution: "WJVRLSJJT" },
+  { day: 6, part: 1, solution: "1282" },
+  { day: 6, part: 2, solution: "3513" },
+  { day: 7, part: 1, solution: "1501149" },
+  { day: 7, part: 2, solution: "10096985" },
+  { day: 8, part: 1, solution: "1703" },
+  { day: 8, part: 2, solution: "496650" },
+  { day: 9, part: 1, solution: "6642" },
+  { day: 9, part: 2, solution: "2765" },
+  { day: 10, part: 1, solution: "13680" },
+  {
+    day: 10,
+    part: 2,
+    solution: `###..####..##..###..#..#.###..####.###..
+#..#....#.#..#.#..#.#.#..#..#.#....#..#.
+#..#...#..#....#..#.##...#..#.###..###..
+###...#...#.##.###..#.#..###..#....#..#.
+#....#....#..#.#....#.#..#....#....#..#.
+#....####..###.#....#..#.#....####.###..`,
+  },
+];
+
+describe("Advent of Code 2022", () => {
+  test.each(solutions)(
+    "Day $day Part $part",
+    async ({ day, part, solution }) => {
+      const result = await solve(day, part);
+      expect(result).toBe(solution);
+    }
+  );
+});
